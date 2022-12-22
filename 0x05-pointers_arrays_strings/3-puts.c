4aed7c6461a40979b738a5eafc085d17590c1696
DistinctionA <ainaadewale23@gmail.com>
2022-12-22 04:02:34 -0800

    Prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * _puts - prints a string
+ * @str: the string
  *
- * Return: the lenght of the string
+ * Return: the length of the string
  */
 void _puts(char *str)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
-	{
 		_putchar(str[i]);
 		i++;
-	}
+
 	_putchar('\n');
 }
